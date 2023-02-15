9bb20d86169cb52da32cb872bfd7cb231932813c
ealbiol <eduard.albiol@gmail.com>
2023-02-15 14:32:11 +0100

    Installinc react semantic ui library

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import { Button } from 'semantic-ui-react'
 
 export default function App() {
-  return (
-    <div>
-        <h1>We are on my site</h1>
-    </div>
-  )
+    return (
+        <div>
+            <h1>We are on my site</h1>
+            <div>
+                <Button primary>Primary</Button>
+                <Button secondary>Secondary</Button>
+            </div>
+        </div>
+    )
 }
