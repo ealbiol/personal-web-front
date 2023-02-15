0bb29ce4972a07291ae0d413947cb93ef285278b
ealbiol <eduard.albiol@gmail.com>
2023-02-15 16:55:12 +0100

    Creating all web public pages

---
+++ src/pages/web/Post/Post.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export function Post() {
+  return (
+    <div>
+        <h1>We are in Post</h1>
+    </div>
+  )
+}
--- src/router/WebRouter.js
+++ src/router/WebRouter.js
@@ -1,8 +1,9 @@
 // PUBLIC USER ZONE ROUTES
 import React from 'react';
 import { Routes, Route } from "react-router-dom";
-import { Home } from "../pages/web";
 import { ClientLayout } from "../layouts"
+import { Home, Contact, Courses, Blog, Post } from "../pages/web";
+
 
 export function WebRouter() {
     // Function charging layout:
@@ -16,6 +17,12 @@ export function WebRouter() {
     return (
         <Routes>
             <Route path="/" element={loadLayout(ClientLayout, Home)} />
+            <Route path="/courses" element={loadLayout(ClientLayout, Courses)} />
+            <Route path="/contact" element={loadLayout(ClientLayout, Contact)} />
+            <Route path="/blog" element={loadLayout(ClientLayout, Blog)} />
+            {/*Dynamic Route with variable 'path'.*/}
+            <Route path="/blog/:path" element={loadLayout(ClientLayout, Post)} />
+
         </Routes>
     )
 }
