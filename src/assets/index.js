5717c10208944f7fd0af2eb0e3ef32fa33c8be08
ealbiol <eduard.albiol@gmail.com>
2023-02-15 14:54:58 +0100

    Importing assets

---
+++ src/assets/index.js
@@ -0,0 +1,25 @@
+// EXPORTING ASSETS
+
+//Svg's:
+import { ReactComponent as LogoWhite } from "./svg/tincode-white.svg";
+
+//Jpg's and png's:
+import authBg from "./jpg/auth-bg.jpg";
+import homeBanner from "./jpg/home-banner.jpg";
+import noAvatar from "./jpg/no-avatar.jpg";
+import AcademyLogo from "./png/academy-logo.png"
+
+//Svg's within Icon const:
+const Icon = {
+    LogoWhite
+};
+
+//Jpg's and png's within image const:
+const image = {
+    authBg,
+    homeBanner,
+    noAvatar,
+    AcademyLogo
+}
+
+export { Icon, image };
