d024e144098912466b38bfaccb22029d5e25b502
ealbiol <eduard.albiol@gmail.com>
2023-02-16 16:42:08 +0100

    Recovering user session.

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,30 +1,42 @@
 // CONTEXT CREATION: Move states globally through all app.
 
 import { useState, useEffect, createContext } from "react";
-import { User } from "../api/user"
-export const AuthContext = createContext();
+import { User, Auth } from "../api"
 
 const userController = new User();
+const authController = new Auth();
+
+export const AuthContext = createContext();
+
 
 export function AuthProvider(props) {
     // The son of the context
     const { children } = props;
     // User state: If null means user is not logged in. If value means is logged in.
-    const [user, setUser] = useState(null)
+    const [user, setUser] = useState(null);
     // Token State: to be used in http petitions
-    const [token, setToken] = useState(null)
-
+    const [token, setToken] = useState(null);
+    // Recover session
+    const [loading, setLoading] = useState(true)
 
     useEffect(() => {
-        // Checking if user is logged in or not.
-    }, [])
+        //Keeping tokens in every render. Self calling function to be able to do async
+        (async () => {
+            // Obtaining both tokens
+            const accessToken = authController.getAccessToken();
+            const refreshToken = authController.getRefreshToken();
+
+            await login(accessToken);
+
+            setLoading(false);
+        })();
+    }, []);
 
     //Login Function: it needs to receive the accessToken
     const login = async (accessToken) => {
         try {
             const response = await userController.getMe(accessToken);
             delete response.password
-            console.log("TOKEN", accessToken);
             setUser({ response }) //User data received
             setToken(accessToken) //accessToken data received in LoginForm.js (login is made a global function)
         } catch (error) {
@@ -38,6 +50,8 @@ export function AuthProvider(props) {
         user, //user state
         login, //login function
     };
+
+    if (loading) return null
 
     //Provider has a property value where we add what we want to export
     return <AuthContext.Provider value={data}>{children}</AuthContext.Provider>
