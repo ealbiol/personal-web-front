6c7c65e12447bec115f0f7a7cba63f696c8d8caf
ealbiol <eduard.albiol@gmail.com>
2023-02-16 19:47:01 +0100

    Menu Structure done

--- src/layouts/AdminLayout/AdminLayout.js
+++ src/layouts/AdminLayout/AdminLayout.js
@@ -1,12 +1,24 @@
-import React from 'react'
+import React from 'react';
+import { Icon } from "../../assets"
+import { AdminMenu } from "../../components/Admin/AdminLayout"
+import "./AdminLayout.scss"
 
 export function AdminLayout(props) {
     const { children } = props;
     return (
-        <div>
-            <h2>Admin Layout is being used</h2>
-
-            {children}
+        <div className='admin-layout'>
+            <div className='admin-layout__left'>
+                <Icon.LogoWhite className='logo' />
+                <AdminMenu/>
+            </div>
+            <div className='admin-layout__right'>
+                <div className='admin-layout__right-header'>
+                    <span>LOGOUT</span>
+                </div>
+                <div className='admin-layout__right-content'>
+                    {children}
+                </div>
+            </div>
         </div>
     )
 }
