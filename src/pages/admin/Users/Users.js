591c041626333c3e798dbef9af532ee108bc6857
ealbiol <eduard.albiol@gmail.com>
2023-02-17 15:11:50 +0100

    Creating user structure page

--- src/pages/admin/Users/Users.js
+++ src/pages/admin/Users/Users.js
@@ -1,10 +1,39 @@
 // USERS PANEL
-import React from 'react'
+import React, { useState } from 'react';
+import { Tab, Button } from "semantic-ui-react"
+import "./Users.scss";
 
 export function Users() {
+  // Tabs: semantic-ui-react own tabs configuration
+  const panes = [
+    {
+      menuItem: "Active users",
+      render: () => (
+        <Tab.Pane attached={false}>
+          <h2>Active Users</h2>
+        </Tab.Pane>
+      )
+    },
+    {
+      menuItem: "Inactive users",
+      render: () => (
+        <Tab.Pane attached={false}>
+          <h2>Inactive Users</h2>
+        </Tab.Pane>
+      )
+    },
+  ]
+
   return (
-    <div>
-        <h1>We are in Users</h1>
-    </div>
+    <>
+      <div className='users-page'>
+        <Button
+          className='users-page__add'
+          primary onClick={() => console.log("Open form")}>
+          New user
+        </Button>
+        <Tab menu={{ secondary: true }} panes={panes} />
+      </div>
+    </>
   )
 }
