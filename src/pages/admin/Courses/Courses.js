d8fd723622b579ec0fdf91e86a3b469e43b48a5a
ealbiol <eduard.albiol@gmail.com>
2023-02-20 23:13:44 +0100

    Courses page structure

--- src/pages/admin/Courses/Courses.js
+++ src/pages/admin/Courses/Courses.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Tab, Button } from "semantic-ui-react";
+import { BasicModal } from "../../../components/Shared";
+import "./Courses.scss";
 
 export function Courses() {
+    const [showModal, setShowModal] = useState(false);
+
+    // Function open/close modal
+    const onOpenCloseModal = () => setShowModal((prevState) => !prevState);
+
     return (
-        <div>
-            <h1>We are in Courses</h1>
-        </div>
+        <>
+            <div className='courses-page'>
+                <div className='courses-page__add'>
+                    <Button primary onClick={onOpenCloseModal}>
+                        New Course
+                    </Button>
+                </div>
+
+                <Tab.Pane attached={false}>
+                    <p>Courses list</p>
+                </Tab.Pane>
+            </div>
+
+            <BasicModal
+                show={showModal}
+                close={onOpenCloseModal}
+                title="Create course"
+            >
+                <p>Creating Course Form</p>
+            </BasicModal>
+        </>
     )
 }
