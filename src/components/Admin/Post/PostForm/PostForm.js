4d1c198702cce176c9bb4678e71f3adbb4de4ef9
ealbiol <eduard.albiol@gmail.com>
2023-02-22 16:03:22 +0100

    Blog Controlling and validating post

--- src/components/Admin/Post/PostForm/PostForm.js
+++ src/components/Admin/Post/PostForm/PostForm.js
@@ -3,13 +3,29 @@ import React, { useCallback } from 'react';
 import { Form, Image } from "semantic-ui-react";
 import { useDropzone } from "react-dropzone";
 import { Editor } from "@tinymce/tinymce-react";
+import { useFormik } from "formik";
+import { initialValues, validationSchema } from "./PostForm.form";
 import "./PostForm.scss";
 
 export function PostForm() {
 
+    const formik = useFormik({
+        initialValues: initialValues(),
+        validationSchema: validationSchema(),
+        validateOnChange: false,
+        onSubmit: async (formValue) => {
+            try {
+                console.log(formValue);
+            } catch (error) {
+                console.error(error);
+            }
+        }
+    })
+
     const onDrop = useCallback((acceptedFile) => {
         const file = acceptedFile[0];
-        console.log(file);
+        formik.setFieldValue("miniature", URL.createObjectURL(file));
+        formik.setFieldValue("file", file);
     });
 
     const { getRootProps, getInputProps } = useDropzone({
@@ -19,34 +35,49 @@ export function PostForm() {
 
     //Function to get miniature
     const getMiniature = () => {
+        if(formik.values.file){
+            return formik.values.miniature;
+        }
         return null;
     }
 
 
     return (
-        <Form className='post-form'>
+        <Form className='post-form' onSubmit={formik.handleSubmit}>
             <Form.Group widths="equal">
-                <Form.Input name="tile" placeholder="Post Title" />
-                <Form.Input name="path" placeholder="Post Path" />
+                <Form.Input
+                    name="title"
+                    placeholder="Post Title"
+                    onChange={formik.handleChange}
+                    value={formik.values.title}
+                    error={formik.errors.title}
+                />
+                <Form.Input
+                    name="path"
+                    placeholder="Post Path"
+                    onChange={formik.handleChange}
+                    value={formik.values.path}
+                    error={formik.errors.path}
+                />
             </Form.Group>
 
 
             {/*Post Editor*/}
             <Editor
-        init={{
-          height: 400,
-          menubar: true,
-          plugins: [
-            "advlist autolink lists link image charmap print preview anchor",
-            "searchreplace visualblocks code fullscreen",
-            "insertdatetime media table paste code help wordcount",
-          ],
-          toolbar:
-            "undo redo | formatselect | bold italic backcolor | \
-             alignleft aligncenter alignright alignjustify | \
-             bullist numlist outdent indent | removeformat | help",
-        }}
-      />
+                init={{
+                    height: 400,
+                    menubar: true,
+                    plugins: [
+                        "advlist autolink lists link image charmap print preview anchor",
+                        "searchreplace visualblocks code fullscreen",
+                        "insertdatetime media table paste code help wordcount",
+                    ],
+                    toolbar:
+                        "undo redo | formatselect | bold italic backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | help",
+                }}
+                initialValue={formik.values.content}
+                onBlur={(e) => formik.setFieldValue("content", e.target.getContent())}
+            />
 
 
             <div className='post-form__miniature' {...getRootProps()}>
@@ -62,7 +93,7 @@ export function PostForm() {
                 )}
             </div>
 
-            <Form.Button type="submit" primary fluid>
+            <Form.Button type="submit" primary fluid loading={formik.isSubmitting}>
                 Create Post
             </Form.Button>
         </Form>
