9ba48e23b481fe86f084b428043ddde57b818e46
ealbiol <eduard.albiol@gmail.com>
2023-02-22 17:40:59 +0100

    Client layout structure

--- src/layouts/ClientLayout/ClientLayout.js
+++ src/layouts/ClientLayout/ClientLayout.js
@@ -1,11 +1,28 @@
-import React from 'react'
+import React from 'react';
+import { Container } from "semantic-ui-react";
+import "./ClientLayout.scss"
 
 export function ClientLayout(props) {
     const { children } = props;
     return (
-        <div>
-            <h2>Client Layout being used</h2>
+        <div className='client-layout'>
+            <div className='client-layout__header'>
+                <p>HEADER</p>
+            </div>
+
             {children}
+
+            <div className='client-layout__footer'>
+                <Container>
+                    <span>INFO</span>
+                    <span>MENU</span>
+                    <span>NEWSLETTER</span>
+                </Container>
+                <Container>
+                    <span>Ⓒ ALL RIGHTS RESERVED</span>
+                    <span>EDUARD ALBIOL | FRONTEND DEVELOPER</span>
+                </Container>
+            </div>
         </div>
     )
 }
