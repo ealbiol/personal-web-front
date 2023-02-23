308a012a56178e95ceb361b5ba43a2cec7f0ab24
ealbiol <eduard.albiol@gmail.com>
2023-02-23 10:57:03 +0100

    Client courses creating pagination

--- src/pages/web/Courses/Courses.js
+++ src/pages/web/Courses/Courses.js
@@ -11,6 +11,12 @@ const courseController = new CourseController();
 export function Courses() {
 
   const [courses, setCourses] = useState(null);
+  const [pagination, setPagination] = useState(null);
+  const [page, setPage] = useState(1);
+  // Function to see whether we are in last page to hide load more button
+  // Here we are checking if the current page is the last one by comparing (pagination.pages meaning the total of pages therefore the last one)
+  // It gives us back true.
+  const isCurrentLastPage = pagination?.page === pagination?.pages
 
   // In console and page /courses
   //console.log(courses);
@@ -18,14 +24,26 @@ export function Courses() {
   useEffect(() => {
     (async () => {
       try {
-        const response = await courseController.getCourses();
-        setCourses(response.docs)
+        const response = await courseController.getCourses({ page, limit: 9 });
+        setPagination({
+          page: response.page,
+          pages: response.pages
+        })
+
+        //Get more pages below instead of only replacing
+        if (!courses) setCourses(response.docs)
+        // we get the current value of courses (...courses) and we add the new value as well (...response.docs)
+        else setCourses([...courses, ...response.docs])
 
       } catch (error) {
         console.error(error);
       }
     })()
-  }, [])
+  }, [page])
+
+  const loadMore = () => {
+    setPage((prevState) => prevState + 1)
+  }
 
   return (
     <Container className='courses-page'>
@@ -43,11 +61,15 @@ export function Courses() {
         ))}
       </div>
 
-      <div className='more'>
-        <Button primary>
-          Load more...
-        </Button>
-      </div>
+      {/*Conditional stating that as long as page is not the last one we show button*/}
+      {!isCurrentLastPage &&
+        <div className='more'>
+          <Button primary onClick={loadMore}>
+            See more...
+          </Button>
+        </div>
+      }
+
     </Container>
   )
 }
