95580db38c3efea07fe0bf181123dcd96f05776c
ealbiol <eduard.albiol@gmail.com>
2023-02-23 10:01:26 +0100

    Client obtaining courses in page courses

--- src/pages/web/Courses/Courses.js
+++ src/pages/web/Courses/Courses.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { Course } from "../../../api";
+import "./Courses.scss";
+
+const courseController = new Course();
 
 export function Courses() {
+
+  const [courses, setCourses] = useState(null);
+
+  // In console and page /courses
+  //console.log(courses);
+  
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await courseController.getCourses();
+        setCourses(response.docs)
+
+      } catch (error) {
+        console.error(error);
+      }
+    })()
+  }, [])
+
   return (
     <div>
-        <h1>We are in Courses</h1>
+      <h1>We are in Courses</h1>
     </div>
   )
 }
