75de36d6b084e14aa35145dce125f8fb0cc4334c
ealbiol <eduard.albiol@gmail.com>
2023-02-24 17:40:02 +0100

    Amending SERVER_IP const

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,8 +1,8 @@
 const SERVER_IP = "webpersonalcourse.herokuapp.com:40222";
 
 export const ENV = {
-    BASE_PATH: `http://${SERVER_IP}`,
-    BASE_API: `http://${SERVER_IP}/api/v1`,
+    BASE_PATH: `https://${SERVER_IP}`,
+    BASE_API: `https://${SERVER_IP}/api/v1`,
     API_ROUTES: {
         //ENDPOINTS ROUTES: Matcing the backend ones
         REGISTER: "auth/register", // API_ROUTES.REGISTER is the same endpoint/url as in insomnia: auth/register
